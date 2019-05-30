70fc335b63118161829983dada996cdb98d6551e
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-05-29 20:10:29 -0500

    error fixed for post method

--- router/register.js
+++ router/register.js
@@ -2,14 +2,17 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const Users= require('../data/userModel.js');
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
     try{
         let user = req.body;
         const hash = bcrypt.hashSync(user.password, 3);
         if(user.username && user.password && user.phone && user.email){
             user.password = hash;
-            Users.add(user)
-            .then(info => {res.status(201).json({message: "Registration Successful", info});})
+            const user = await Users.add(user);
+            if(user){
+              res.status(201).json({message: "Registration Successful", user})
+            }
+
         } else {
             res.status(402).json({error: 'Please provide a Username, Password, Email and Phone number'})
         }
